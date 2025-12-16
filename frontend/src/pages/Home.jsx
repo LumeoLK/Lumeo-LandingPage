c1640437d7d24873cb5b78117a1aacbcb55b3f3a
Maneth-k <manethkaveen0@gmail.com>
2025-12-16 11:43:33 +0530

    trying deployment 1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,15 +3,16 @@ import GetInTouch from "../components/GetInTouch.jsx";
 import FeaturesHome from "../components/FeaturesHome.jsx";
 import Hero from "../components/Hero.jsx";
 import Navbar from "../components/NavBar.jsx";
+import NeonGridScene from "../components/NeonGridScene.jsx";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <NeonGridScene />
       <Hero />
       <FeaturesHome />
       <GetInTouch />
-
     </div>
   );
 };
